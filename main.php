98d363ed64d4bb09211c503135cd0f9c62faf161
Reid Beels <mail@reidbeels.com>
2009-02-26 01:53:18 -0800

    Updating layout to include registration links

--- main.php
+++ main.php
@@ -14,29 +14,36 @@
    <div id="header">
       <h1 id='site-title'><?php tpl_link(wl(),$conf['title'],'name="top" accesskey="h" title="[ALT+H]"') ?></h1>
       <div id="site-description">An open source conference in Portland, Oregon | June 17-19, 2009</div>
+      <div id='conference-date-location'><p>June 17&ndash;19, 2009 - Portland, OR</p></div>
     </div>
     
-    <div id="access">
-       <div class="skip-link">
-         <a href="#content" title="Skip to content">Skip to content</a>
-       </div>
-       <div id="menu">
+   <div id="access">
+    <div class="skip-link">
+      <a href="#content" title="Skip to content">Skip to content</a>
+    </div>
+
+    <div id="menu">
          <ul>
-           <li><a href="/">Blog</a>
-           <li class="page_item page-item-10"><a href="/about/" title="About">About</a></li>
-           <li class="page_item page-item-22"><a href="/contact-us/" title="Contact Us">Contact Us</a></li>
-           <li class="page_item page-item-56"><a href="/volunteer/" title="Volunteer">Volunteer</a></li>
-           <li class="page_item"><a href="/sponsors/" title="Sponsors">Sponsors</a></li>
-           <li class="page_item"><a href="/proposals/">Proposals</a></li>
+            <li><a href='/attend/' title='Attend' class=''>Attend</a></li>
+            <li><a href='/proposals/' title='Proposals' class=''>Proposals</a></li>
+            <li><a href="/volunteer/" title="Volunteer">Get Involved</a></li>
+            <li><a href="/sponsors/" title="Sponsors">Sponsors</a></li>
+            <li><a href="/">Blog</a></li>
          </ul>
-       </div>
+      </div>
+      
+      <div id='menu-sidebar-background'></div>
+      <div id='menu-sidebar-nav'>
+         <a href='/attend' title='Register Now' class='register-now-button'>Register Now</a>
+         <a href='/proposals' title='Submit a Proposal' class='submit-a-proposal-button'>Submit a Proposal</a>
+      </div>
    </div>
-   	
+      
    <div id="container">
       <?php flush()?>
 
       <div id="content">
-  		  <?php html_msgarea()?>
+        <?php html_msgarea()?>
         <?php tpl_content()?>
         <div class="meta">
            <div class="doc">
@@ -71,7 +78,7 @@
          <div class="searchform"> <?php tpl_searchform() ?> </div>
       </li>
       <?php } ?>
-     	<?php tpl_indexerWebBug();?>
+      <?php tpl_indexerWebBug();?>
 </div>
 
   <div id='footer'></div>
